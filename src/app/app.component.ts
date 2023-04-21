385cd9148942d7f6af638fb02f5d956b2dcb058a
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-20 22:26:42 -0300

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-recipes-app';
+  isAddRecipeVisible = false;
+  recipeName = '';
+  recipeDescription = '';
+  recipeImage = '';
+  ingredientName = '';
+  ingredientAmount = '';
+  ingredients: { name: string; amount: string }[] = [];
+  recipes: {
+    name: string;
+    description: string;
+    image: string;
+    ingredients: { name: string; amount: string }[];
+  }[] = [];
+
+  onAddRecipe() {
+    this.isAddRecipeVisible = true;
+  }
+
+  onIngredientAdded() {
+    this.ingredients.push({
+      name: this.ingredientName,
+      amount: this.ingredientAmount,
+    });
+    this.ingredientName = '';
+    this.ingredientAmount = '';
+  }
+
+  onRecipeCancel() {
+    this.isAddRecipeVisible = false;
+  }
+
+  onRecipeAdded() {
+    this.recipes.push({
+      name: this.recipeName,
+      description: this.recipeDescription,
+      image: this.recipeImage,
+      ingredients: this.ingredients,
+    });
+    this.recipeName = '';
+    this.recipeDescription = '';
+    this.recipeImage = '';
+    this.ingredients = [];
+    this.isAddRecipeVisible = false;
+  }
 }
